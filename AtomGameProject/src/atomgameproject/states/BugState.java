6157d41c88d065d908ab67d47963f0ac0c19edc5
Johnny <horsentp@gmail.com>
2013-08-04 14:58:56 -0500

    Bug state

--- AtomGameProject/src/atomgameproject/states/BugState.java
+++ AtomGameProject/src/atomgameproject/states/BugState.java
@@ -5,38 +5,81 @@
 package atomgameproject.states;
 
 import atomgameproject.AtomGameMain;
+import atomgameproject.ImageLoader;
+import atomgameproject.launcher.config.KeyBindWrapper;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import simplegames.animation.GameDrawingPanel;
+import simplegames.inputlisteners.MouseDownListener;
+import simplegames.inputlisteners.MouseMovedListener;
+import simplegames.inputlisteners.click.ClickAreaListener;
+import simplegames.inputlisteners.click.HoverImageClickArea;
 import simplegames.states.GameState;
 
 /**
  *
  * @author Jonathon
  */
-public class BugState extends GameState {
+public class BugState extends GameState implements MouseDownListener {
 
+    private KeyBindWrapper wrapper;
+    private HoverImageClickArea exitButton;
+    
+    public BugState(KeyBindWrapper wrapper) {
+        this.wrapper = wrapper;
+    }
+    
     @Override
     public void init(GameDrawingPanel pnl) {
+        int width = AtomGameMain.frameWidth;
+        int height = AtomGameMain.frameHeight;
+        exitButton = new HoverImageClickArea((width/2)-(275/2), (height/2)+(height/6), ImageLoader.loadImage("img/buttons/orangeButtonDown.png"), ImageLoader.loadImage("img/buttons/orangeButtonUp.png"));
     }
 
     @Override
     public void registerListeners(GameDrawingPanel pnl) {
+        exitButton.addListener(new BackToStartListener(pnl));
+        pnl.addMouseMovedListener(exitButton);
+        pnl.addMouseDownListener(this);
     }
 
     @Override
     public BufferedImage drawToImage(BufferedImage bi, Graphics2D gd) {
         gd = (Graphics2D) bi.createGraphics();
+        bi = exitButton.draw(bi, gd);
         gd.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 18));
         gd.setColor(Color.BLUE);
         gd.drawString("Sometimes the bullets stop working", (AtomGameMain.frameWidth/2)-220, 50);
+        gd.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 40));
+        gd.setColor(Color.BLACK);
+        gd.drawString("To Main", (int)exitButton.getBox().getX()+50, (int)exitButton.getBox().getY()+50);
         return bi;
     }
 
     @Override
     public void updateState(float f) {
+        
+    }
+
+    @Override
+    public void reactToMouseDown(int x, int y, MouseEvent me) {
+        exitButton.checkSpot(x, y);
+    }
+    
+    class BackToStartListener implements ClickAreaListener {
+        private GameDrawingPanel parent;
+        public BackToStartListener(GameDrawingPanel parent) {
+            this.parent = parent;
+        }
+        
+        @Override
+        public void reactToClick() {
+            parent.changeState(new StartState(wrapper));
+        }
+        
     }
     
 }
--- AtomGameProject/src/atomgameproject/states/StartState.java
+++ AtomGameProject/src/atomgameproject/states/StartState.java
@@ -24,7 +24,7 @@ public class StartState extends GameState implements MouseDownListener {
     
     private GameDrawingPanel panel;
     protected KeyBindWrapper bindings;
-    private HoverImageClickArea startButton, exitButton;
+    private HoverImageClickArea startButton, exitButton, bugButton;
     
     public StartState(KeyBindWrapper bindings) {
         this.bindings = bindings;
@@ -37,14 +37,17 @@ public class StartState extends GameState implements MouseDownListener {
         int height = AtomGameMain.frameHeight;
         startButton = new HoverImageClickArea((width/2)-(275/2), (height/2)-(height/6), ImageLoader.loadImage("img/buttons/purpleButtonDown.png"), ImageLoader.loadImage("img/buttons/purpleButtonUp.png"));
         exitButton = new HoverImageClickArea((width/2)-(275/2), (height/2)+(height/6), ImageLoader.loadImage("img/buttons/orangeButtonDown.png"), ImageLoader.loadImage("img/buttons/orangeButtonUp.png"));
+        bugButton = new HoverImageClickArea((width/2)-(275/2), (height/2), ImageLoader.loadImage("img/buttons/blueButtonDown.png"), ImageLoader.loadImage("img/buttons/blueButtonUp.png"));
     }
 
     @Override
     public void registerListeners(GameDrawingPanel panel) {
         startButton.addListener(new StartGameListener(panel));
         exitButton.addListener(new ExitGameListener(panel));
+        bugButton.addListener(new BugGameListener(panel));
         panel.addMouseMovedListener(startButton);
         panel.addMouseMovedListener(exitButton);
+        panel.addMouseMovedListener(bugButton);
         panel.addMouseDownListener(this);
     }
 
@@ -53,6 +56,7 @@ public class StartState extends GameState implements MouseDownListener {
         graphics2d = (Graphics2D) bImage.getGraphics();
         bImage = startButton.draw(bImage, graphics2d);
         bImage = exitButton.draw(bImage, graphics2d);
+        bImage = bugButton.draw(bImage, graphics2d);
         graphics2d.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 72));
         graphics2d.setColor(Color.GREEN);
         graphics2d.drawString("Atom Game", (AtomGameMain.frameWidth/2)-190, (AtomGameMain.frameHeight/6));
@@ -63,6 +67,7 @@ public class StartState extends GameState implements MouseDownListener {
         graphics2d.setColor(Color.BLACK);
         graphics2d.drawString("Start", (int)startButton.getBox().getX()+70, (int)startButton.getBox().getY()+50);
         graphics2d.drawString("Exit", (int)exitButton.getBox().getX()+70, (int)exitButton.getBox().getY()+50);
+        graphics2d.drawString("Known Bugs", (int)bugButton.getBox().getX()+5, (int)bugButton.getBox().getY()+50);
         return bImage;
     }
 
@@ -75,6 +80,7 @@ public class StartState extends GameState implements MouseDownListener {
     public void reactToMouseDown(int x, int y, MouseEvent me) {
         startButton.checkSpot(x, y);
         exitButton.checkSpot(x, y);
+        bugButton.checkSpot(x, y);
     }
     
     class ExitGameListener implements ClickAreaListener {
@@ -101,6 +107,20 @@ public class StartState extends GameState implements MouseDownListener {
         @Override
         public void reactToClick() {
             panel.changeState(new PlayState(bindings));
+        }
+        
+    }
+    
+    class BugGameListener implements ClickAreaListener {
+
+        private GameDrawingPanel panel;
+        public BugGameListener(GameDrawingPanel panel) {
+            this.panel = panel;
+        }
+        
+        @Override
+        public void reactToClick() {
+            panel.changeState(new BugState(bindings));
         }
         
     }
